cc9938723d755954359da3d5845724a1b93c480c
makhizu <makhi.guebro@gmail.com>
2022-12-26 17:00:28 +0700

    Update makhi_post.sql

--- makhi_post.sql
+++ makhi_post.sql
@@ -14,7 +14,27 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+-- Dumping structure for table dbtubes.makhi_posts
+CREATE TABLE IF NOT EXISTS `makhi_posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) NOT NULL,
+  `body` text NOT NULL,
+  `pic` varchar(100) DEFAULT NULL,
+  `excerpt` varchar(250) DEFAULT NULL,
+  `excerpt_awal` varchar(100) DEFAULT NULL,
+  `user_id` smallint DEFAULT NULL,
+  `datecreated` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table dbtubes.makhi_posts: ~6 rows (approximately)
+REPLACE INTO `makhi_posts` (`id`, `title`, `body`, `pic`, `excerpt`, `excerpt_awal`, `user_id`, `datecreated`) VALUES
+	(1, 'aaaaaaaaaaaaaaaa', '<div>aaaaaaaaaaaaaaaaaaaaaaaaaa</div>', NULL, '<div>aaaaaaaaaaaaaaaaaaaaaaaaaa</div>', '<div>aaaaaaaaaaaaaaaaaaaaaaaaaa</div>', 3, '2022-12-26'),
+	(2, 'aaaaaaaaaaaaaaaabbb', '<div>bdbdfgfhfgjhfdgerdfvrdfgrfgtrferfgrtfgrshf</div>', NULL, 'bdbdfgfhfgjhfdgerdfvrdfgrfgtrferfgrtfgrshf', 'bdbdfgfhfgjhfdgerdfvrdfgrfgtrferfgrtfgrshf', 3, '2022-12-26'),
+	(3, 'aaaaaaaaaaaaaaaaa', '<div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>', NULL, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', NULL, '2022-12-26'),
+	(4, 'sdgfhgjhkjkl,;.', '<div>szdfxcgb,nm.,./ghhgjgkgjkhjjhkjlkjftfrutmtr,trmgrt,gnrrnrjrw4hwardstrd,esreytsjresm</div>', NULL, 'szdfxcgb,nm.,./ghhgjgkgjkhjjhkjlkjftfrutmtr,trmgrt,gnrrnrjrw4hwardstrd,esreytsjresm', 'szdfxcgb,nm.,./ghhgjgkgjkhjjhkjlkjftfrutmtr,t', 3, '2022-12-26'),
+	(5, 'werthhfgersrshghtrwhrsf', '<div>fwafdgshdtjtdheafgaehewyaehrstgaethesgaesgdsgsrwarfhrwreataeefafafdgae</div>', NULL, 'fwafdgshdtjtdheafgaehewyaehrstgaethesgaesgdsgsrwarfhrwreataeefafafdgae', 'fwafdgshdtjtdheafgaehewyaehrstgaethesgaesgdsg', 3, '2022-12-26'),
+	(7, 'ffffffffffffffffffffffffffffffffffffffff', '<div>fffffffffffffffffffffffff fffffffffffffffffffffffffffffffffffffffffff fffffffffffffffffffffffffffffffffffffff rfffffffffffffffffffffffff fffffffffffff</div>', NULL, 'fffffffffffffffffffffffff fffffffffffffffffffffffffffffffffffffffffff fffffffffffffffffffffffff', 'fffffffffffffffffffffffff fffffffffffffffffff', 3, '2022-12-26');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
