f7914c5b0a98c0822228dd57d7288f15684259be
Alghifary571 <75029996+Alghifary571@users.noreply.github.com>
2022-12-28 11:26:32 +0700

    Perubahan view

--- application/views/alghi/VAddNew.php
+++ application/views/alghi/VAddNew.php
@@ -4,21 +4,29 @@
   <?php $this->load->view("alghi/partials/head.php") ?>
 </head>
 <body>
+  <div class="col-11 mx-auto">
 <form action="<?php echo site_url('alghi/AddNew/Add') ?>" method="post" enctype="multipart/form-data" >
   <div class="form-group">
     <label for="IsiJudul">Judul</label>
     <input name="Judul" type="text" class="form-control" id="IsiJudul"placeholder="Isi Judul">
   </div>
+</div>
+  <div class="col-11 mx-auto">
   <div class="form-group">
     <label for="IsiKategori">Kategori</label>
     <input name="Kategori" type="text" class="form-control" id="IsiKategori" placeholder="Kategori">
   </div>
+</div>
+  <div class="col-11 mx-auto">
   <div class="form-group">
     <label for="IsiKonten">Isi</label>
     <textarea name="Isi" type="text" class="form-control" id="IsiKonten" placeholder="Konten"></textarea>
     <!-- <input name="Isi" type="text" class="form-control" id="IsiKonten" placeholder="Konten"> -->
   </div>
+</div>
+  <div class="col-11 mx-auto">
   <button type="submit" class="btn btn-primary">Submit</button>
+</div>
 </form>
 </body>
 </html>
--- application/views/alghi/VEdit.php
+++ application/views/alghi/VEdit.php
@@ -4,23 +4,31 @@
   <?php $this->load->view("alghi/partials/head.php") ?>
 </head>
 <body>
+  <div class="col-11 mx-auto">
 <form action="<?php echo site_url('/alghi/AddNew/Update/') ?>" method="post" enctype="multipart/form-data" >
   <div class="form-group">
     <label for="IsiJudul">Judul</label>
     <input name="Judul" type="text" class="form-control" id="IsiJudul"placeholder="Isi Judul" value="<?php echo $post['Judul'] ?>">
     <input type="hidden" name="id_posts" value="<?php echo $post['id_posts'] ?>" />
   </div>
+</div>
+<div class="col-11 mx-auto">
   <div class="form-group">
     <label for="IsiKategori">Kategori</label>
     <input name="Kategori" type="text" class="form-control" id="IsiKategori" placeholder="Kategori" value="<?php echo $post['Kategori'] ?>">
     <input type="hidden" name="id_kategori" value="<?php echo $post['id_kategori'] ?>" />
   </div>
+</div>
+  <div class="col-11 mx-auto">
   <div class="form-group">
     <label for="IsiKonten">Isi</label>
     <textarea name="Isi" type="text" class="form-control" id="IsiKonten" placeholder="Konten"><?php echo $post['Isi'] ?></textarea>
   </div>
+</div>
   <br>
+  <div class="col-11 mx-auto">
   <button type="submit" class="btn btn-primary">Submit</button>
+</div>
 </form>
 </body>
 </html>
