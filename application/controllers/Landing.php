7d53f0fd8bcd229abdf637aee94e0ec1f2b97879
makhizu <makhi.guebro@gmail.com>
2023-01-10 22:53:03 +0700

    add assets buat landing page
    sama edit controller landing

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Landing extends CI_Controller {
+class Landing extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,6 @@ class Landing extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('landing');
+		$this->load->view('landingpage');
 	}
 }
